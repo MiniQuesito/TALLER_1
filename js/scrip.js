342cf3033c697a109393405b5c3e4a332cd28b3b
Arnold <arnoldretavisca68@gmail.com>
2022-08-04 11:33:23 -0500

    Se realizo los condicionales

--- js/scrip.js
+++ js/scrip.js
@@ -14,11 +14,66 @@ let boleana= "FALSE" ;
 let Sena=" ";
 
 console.log(`La variable boleana es ${boleana} y la otra variable es ${Sena}`);
-
+/*
 let res=numero+5;
 console.log("El resultado es " ,+res);
 console.log(`El resultado es ${res}`);
 console.log(`El resultado es ${numero+5}`);
-
+*/
 //window.alert("Esto es una alerta");
-window.confirm("Estas seguro de abandonar la partida?");
+/*
+let estado=window.confirm("Estas seguro de abandonar la partida?");
+console.log(`La ventana arrojo ${estado}`);
+
+let num1=prompt("Digite el primer numero");
+let res1=numero+paseInt(num1);
+console.log(`El resultado es ${res1}`);
+*/
+
+let numero1=prompt("digite el primer numero");
+let numero2=prompt("Digite el segundo numero");
+let resultado=parseInt(numero1)+parseInt(numero2);
+
+//console.log(`La suma de los dos numeros es ${resultado}`);
+
+//Condicionales
+/*
+if(numero1>numero2){
+    console.log(`El numero ${numero1} es mayor`);
+}
+else if(numero1==numero2){
+    console.log(`Los numeros ${numero1} y ${numero2} son iguales`);
+}
+else{
+    console.log(`El numero ${numero2} es mayor`);
+}
+*/
+
+//Operador terniario
+/*
+let nummayor=(numero1>numero2)
+?"El numero mayor es el primero"
+: "El numero mayor es el segundo"
+
+console.log(nummayor);
+*/
+/*
+let usuario=prompt("Digite su usuario");
+let contraseña=prompt("Digite su contraseña");
+
+if(usuario=="Arnold" && contraseña=="qwerty"){
+    window.alert("Bienvenido");
+}
+else if(usuario=="Arnold" && contraseña!="qwerty"){
+    window.alert("Verifique su contraseña");
+}
+else if(usuario!="Arnold" && contraseña=="qwerty"){
+    window.alert("Verifique su Usuario");
+}
+else if(usuario!="Arnold" && contraseña!="qwerty"){
+    window.alert("sus credenciales de accseso son incorrectas");
+}
+*/
+//condicional switch 
+
+
