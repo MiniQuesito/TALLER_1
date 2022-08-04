cf4b5ec8d6f21c38e5dd46994bc4d3a07cb12d54
Arnold <arnoldretavisca68@gmail.com>
2022-08-04 08:42:57 -0500

    Se creo los documentos base para el proyecto

---
+++ js/scrip.js
@@ -0,0 +1,24 @@
+console.log("Hola Mundo- PvP julian??!!!"); 
+
+let numero=25
+
+console.log("El numero es " ,numero);
+console.log(`el numero es ${numero}`);
+
+let nombre="Arnold";
+
+console.log(`El nombre es ${nombre} y el numero es ${numero}`);
+
+let boleana= "FALSE" ;
+
+let Sena=" ";
+
+console.log(`La variable boleana es ${boleana} y la otra variable es ${Sena}`);
+
+let res=numero+5;
+console.log("El resultado es " ,+res);
+console.log(`El resultado es ${res}`);
+console.log(`El resultado es ${numero+5}`);
+
+//window.alert("Esto es una alerta");
+window.confirm("Estas seguro de abandonar la partida?");
