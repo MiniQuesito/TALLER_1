fe46e91436b99c1ea7e8176774f608e33e3a9d55
Arnold <arnoldretavisca68@gmail.com>
2022-08-23 12:28:36 -0500

    se realizo los talleres uno y dos

---
+++ js/ejercicioTaller1.js
@@ -0,0 +1,27 @@
+/*
+ La longitud de la palabra
+ La palabra convertida en mayúsculas
+ La palabra convertida en minúsculas
+ El primer carácter de la palabra */ 
+
+var cualquierCadena="";
+
+cualquierCadena=prompt("Ingrese una cadena de texto");
+
+cualquierCadena.foreach(cualquierCadena => console.log("El carácter en el índice 0 es '" + cualquierCadena.charAt(0) + "'"))
+
+
+/*
+console.log("El carácter en el índice 0 es '" + cualquierCadena.charAt(0) + "'")
+console.log("El carácter en el índice 1 es '" + cualquierCadena.charAt(1) + "'")
+console.log("El carácter en el índice 2 es '" + cualquierCadena.charAt(2) + "'")
+console.log("El carácter en el índice 3 es '" + cualquierCadena.charAt(3) + "'")
+console.log("El carácter en el índice 4 es '" + cualquierCadena.charAt(4) + "'")
+console.log("El carácter en el índice 5 es '" + cualquierCadena.charAt(5) + "'")
+console.log("El carácter en el índice 6 es '" + cualquierCadena.charAt(6) + "'")
+console.log("El carácter en el índice 7 es '" + cualquierCadena.charAt(7) + "'")
+console.log("El carácter en el índice 8 es '" + cualquierCadena.charAt(8) + "'")
+console.log("El carácter en el índice 9 es '" + cualquierCadena.charAt(9) + "'")
+console.log("El carácter en el índice 10 es '" + cualquierCadena.charAt(10) + "'")
+console.log("El carácter en el índice 11 es '" + cualquierCadena.charAt(11) + "'")
+*/
